10d68485f4c10d34d96a31376a5de9f4731996d0
KumarJonnala <kumarsai.phoenix@gmail.com>
2026-04-20 15:24:40 +0200

    slrum file

--- Modular/slurm_fake_review_llm.sh
+++ Modular/slurm_fake_review_llm.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 #SBATCH --job-name=fake_review_llm
 #SBATCH --partition=gpu
+#SBATCH --nodelist=ant2
 #SBATCH --output=job_%j.out
 #SBATCH --error=job_%j.err
 
-#SBATCH --gres=gpu:1
+#SBATCH --gres=shard:8
 #SBATCH --cpus-per-task=8
 #SBATCH --mem-per-cpu=2G
-#SBATCH --time=02:00:00
+#SBATCH --time=06:00:00
 
 pip install -r requirements.txt
-srun python3 langchain_prompting.py
+python3 langchain_prompting.py
